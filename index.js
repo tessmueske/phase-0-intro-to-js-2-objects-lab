50aafa51d7b9cd6b47edd884df5dfc7f701127ec
Tess <tess.mueske@gmail.com>
2023-11-12 13:18:53 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+class Employee {
+    constructor() {
+    }
+  }
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+
+  const employee = new Employee();
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, key, value);
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const clone = { ...employee }; 
+    delete clone[key];
+    clone[key] = undefined; 
+    return clone;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+
